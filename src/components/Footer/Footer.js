79b314a0f69f7e86642172bcd4a1b41503ec8648
Diego Guerrero Alexander <diego@klar.mx>
2021-05-23 17:41:35 -0500

    setup/creationOfInitialComponents

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li>@kontraer.berlin</li>
+                <li>Winsstraße 53, Berlin</li>
+                <li>hELLO@kontrar.com</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
